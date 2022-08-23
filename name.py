7f10e5dfd97f5bdccd528068878c972ab16e6028
Wilhelm <jlucking1310@gmail.com>
2022-08-23 10:25:12 -0400

    This file is for variables and simple data types

---
+++ name.py
@@ -0,0 +1,22 @@
+name = "paul atreides"
+print(name.title())
+
+first_name = "duncan"
+last_name = "idaho"
+
+full_name = first_name + " " + last_name
+
+print(full_name.title())
+
+message = "This is your message"
+message2 = "This is your message on TAB"
+
+print(message)
+print("\t" + message2)
+
+favoritePlanet = "Arrakis. "
+print("Hello, my favorite planet in the Dune universe is " + favoritePlanet)
+
+favoritePlanet = favoritePlanet.rstrip()
+
+print(favoritePlanet)
