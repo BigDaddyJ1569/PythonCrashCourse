52590288c324cd3742ff41a7a2613709c7080235
Wilhelm <jlucking1310@gmail.com>
2022-08-24 10:05:28 -0400

    How Python handles numbers, math, and them in strs

--- Numbers.py
+++ Numbers.py
@@ -15,10 +15,23 @@ print(eq2)
 eq3 = 4 * 25
 print(eq3)
 
-#Simple division, results in 5.5
+#Simple division, results in a float of 5.5
 eq4 = 11 / 2
 print(eq4)
 
 #Python does support PEMDAS, so it will solve the division first then perform the addition
 eq5 = 69 + 34/22
 print(eq5)
+
+
+#A Lesson on avoiding Type errors within the str() function
+#
+#Line 35 will display the error on line 31 because you cannot cat an int into a string. It will produce a type error.
+#
+#Exception has occurred: TypeErrorcan only concatenate str (not "int") to str
+#
+#
+age = 21
+#message = "Happy " + age + "st birthday!"
+message = "Happy " + str(age) + "st birthday!"
+print(message)
