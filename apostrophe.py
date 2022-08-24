4cceb6cc3381d60791e77c63602f5b5e5fbcf965
Wilhelm <jlucking1310@gmail.com>
2022-08-24 09:12:48 -0400

    Learning about quotation marks and their syntax

--- apostrophe.py
+++ apostrophe.py
@@ -1,2 +1,9 @@
 message = "One of Python's strengths is its diverse community."
 print(message)
+
+#the lines below would not work (can display a SyntaxError) because of how Python interpret's text within the different quotation marks.
+#This happens because Python interprets everything between the first single quote and the apostrophe as a string. It then tries to interpret the rest of the text as Python code, which 
+# causes errors.
+
+#messageWrong = 'One of Python's strengths is its diverse community.'
+#print(messageWrong)
